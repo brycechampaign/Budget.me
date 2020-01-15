e9bfec941a664d4f91a94fa59b707cf04a87e0cd
Bryce Champaign <bechampaign@protonmail.com>
2020-01-15 18:25:10 -0500

    Implement database schemas and export models based on each schema
    Schemas for transactions (Tx), Months, and Budgets have been defined and exported.

--- server/db/index.js
+++ server/db/index.js
@@ -6,3 +6,27 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('database connected');
 });
+
+const txSchema = new mongoose.Schema({
+  date: Date,
+  category: String,
+  amount: Number,
+  recipient: String,
+  notes: String
+});
+
+const monthSchema = new mongoose.Schema({
+  goal: Number,
+  transactions: [txSchema]
+});
+
+const budgetSchema = new mongoose.Schema({
+  user: String,
+  months: [monthSchema]
+});
+
+module.exports = {
+  Tx: mongoose.model('Tx', txSchema),
+  Month: mongoose.model('Month', monthSchema),
+  Budget: mongoose.model('Budget', budgetSchema)
+};
