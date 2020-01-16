c59780a7375d7953048cb485973b4a295535b464
Bryce Champaign <bechampaign@protonmail.com>
2020-01-16 18:56:33 -0500

    Add css selectors, reformat elements, and add human-readable months

--- src/components/Tx.jsx
+++ src/components/Tx.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
 
+const getMonthString = num => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  return months[num];
+};
+
 const Tx = ({ date, category, amount, recipient, notes = null }) => {
+  const dateObj = new Date(date);
+  const isIncome = category === 'income';
   return (
     <div className="tx">
-      <p>
-        {new Date(date).toLocaleString('en-US', {
-          dateStyle: 'medium'
-        })}
-      </p>
-      <div styles={{ display: 'flex', flexDirection: 'column' }}>
-        <p>{recipient}</p>
-        <p>{category}</p>
+      <div id="tx-date">
+        <p>{getMonthString(dateObj.getMonth())}</p>
+        <p>{dateObj.getDay()}</p>
       </div>
-      <p>{amount}</p>
+      <div
+        className="tx-middle"
+        styles={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <p>{recipient}</p>
+        <p id="category">{category}</p>
+      </div>
+      <p
+        className="tx-amount"
+        style={isIncome ? { color: 'green' } : { color: 'red' }}
+      >
+        {isIncome ? '+' : '-' + '$' + amount.toFixed(2)}
+      </p>
     </div>
   );
 };
