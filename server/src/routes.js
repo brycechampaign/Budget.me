8df87fa33a0163feb733c0bfcd9a5389b063f448
Bryce Champaign <bechampaign@protonmail.com>
2020-01-16 10:51:33 -0500

    Implement handling for POST requests to /transactions/:user
    Requests to POST at the /transactions/:user endpoint will be able to append transactions to the budget of the user specified in the url parameter.

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
-const { getUserTxnsInMonth } = require('./controllers.js');
+const { getUserTxnsInMonth, addTransaction } = require('./controllers.js');
 
 router.get('/transactions/:user/:month', (req, res) => {
   const { user, month } = req.params;
   getUserTxnsInMonth(user, Number(month))
     .then(txns => res.send(txns))
+    .catch(err => console.error(err));
+});
+
+router.post('/transactions/:user', (req, res) => {
+  const { user } = req.params;
+  const { category, amount, recipient, notes } = req.body;
+
+  addTransaction(user, category, amount, recipient, notes, (date = new Date()))
+    .then(() => res.sendStatus(201))
     .catch(err => console.error(err));
 });
 
