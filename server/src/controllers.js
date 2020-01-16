a2042e3d4cb48bf5791dc6b3e098b5f8fc76ecf6
Bryce Champaign <bechampaign@protonmail.com>
2020-01-16 10:50:04 -0500

    Implement model and controller functions for adding transactions to the database
    AddTransaction accepts user, category, amount, recipient, notes, and date arguments and adds a transaction with those properties to the database under the budget of the input user.

--- server/src/controllers.js
+++ server/src/controllers.js
@@ -1,5 +1,16 @@
-const { getUserTxsInMonth } = require('../models.js');
+const { getUserTxsInMonth, addTransaction } = require('./models.js');
 
-module.exports.getUserTxnsInMonth = (user, month) => {
+module.exports.getUserTxnsInMonth = user => {
   return getUserTxsInMonth(user, month);
+};
+
+module.exports.addTransaction = (
+  user,
+  category,
+  amount,
+  recipient,
+  notes,
+  date
+) => {
+  return addTransaction(user, category, amount, recipient, notes, date);
 };
---
+++ server/src/models.js
@@ -0,0 +1,60 @@
+const { Tx, Month, Budget } = require('../db/index');
+
+module.exports.createBudget = (user, goal, ...transaction) => {
+  categories = [
+    { name: 'coffee', limit: 20 },
+    { name: 'Fun times', limit: 10 }
+  ];
+
+  const month = new Month({
+    goal,
+    num: new Date().getMonth()
+  });
+
+  const budget = new Budget({
+    user,
+    months: [month],
+    categories
+  });
+
+  budget.save();
+};
+
+module.exports.getUserTxsInMonth = async (user, month) => {
+  const budget = await Budget.findOne({ user });
+
+  for (let i = 0; i < budget.months.length; i++) {
+    const currMonth = budget.months[i];
+    if (currMonth.num === month) return currMonth.transactions;
+  }
+
+  throw new Error('No transactions found for given month');
+};
+
+module.exports.addTransaction = async (
+  user,
+  category,
+  amount,
+  recipient,
+  notes,
+  date
+) => {
+  const budget = await Budget.findOne({ user });
+  const month = date.getMonth();
+  date = date.toLocaleString('en-US');
+
+  for (let i = 0; i < budget.months.length; i++) {
+    const currMonth = budget.months[i];
+    if (currMonth.num === month) {
+      budget.months[i].transactions.push(
+        new Tx({ category, amount, recipient, notes, date })
+      );
+
+      budget.save(err => {
+        throw new Error('Error saving transaction');
+      });
+
+      return;
+    }
+  }
+};
