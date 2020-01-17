2ac78c746c54640b58864d18a7134be9c1dba710
Bryce Champaign <bechampaign@protonmail.com>
2020-01-16 23:32:56 -0500

    Implement sorting by date for user transactions

--- server/src/controllers.js
+++ server/src/controllers.js
@@ -1,7 +1,23 @@
 const { getUserTxsInMonth, addTransaction, getGoal } = require('./models.js');
 
-module.exports.getUserTxnsInMonth = (user, month) => {
-  return getUserTxsInMonth(user, month);
+module.exports.getUserTxnsInMonth = async (user, month) => {
+  let transactions = await getUserTxsInMonth(user, month);
+
+  transactions.sort((a, b) => {
+    const dateA = new Date(a.date).getDate();
+    const dateB = new Date(b.date).getDate();
+
+    if (dateA > dateB) {
+      return -1;
+    }
+    if (dateB > dateA) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return transactions;
 };
 
 module.exports.addTransaction = (
