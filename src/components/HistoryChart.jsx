5162c6dd866f54f323fb6a0332b3486c2bc8bc3c
Bryce Champaign <bechampaign@protonmail.com>
2020-01-16 23:57:28 -0500

    Replace ChartJS chart with Chartist chart
    Upon researching after experiencing browser freezes I discoverd that chartJS seems to have a bug where it will slowly freeze the browser. The solution was to unimplement ChartJS and replace
    it with the Chartist library.

--- src/components/HistoryChart.jsx
+++ src/components/HistoryChart.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import Tx from './Tx';
+import ChartistGraph from 'react-chartist';
+import Chartist from 'chartist';
 
 const HistoryChart = ({ transactions, budget }) => {
   let labels = [];
@@ -30,39 +32,21 @@ const HistoryChart = ({ transactions, budget }) => {
     newData.push(newBudget);
   }
 
-  console.log(newData);
-
   const dataSet = {
     labels,
-    datasets: [
-      {
-        label: 'Balance',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: newData
-      }
-    ]
+    series: [newData]
   };
 
   if (transactions !== null) {
+    const options = {
+      height: '40vh'
+    };
+
+    const type = 'Line';
     return (
       <div className="card" id="chart-container">
-        <Line data={dataSet} />
+        <h2>Your Month so Far</h2>
+        <ChartistGraph data={dataSet} type={type} options={options} />
       </div>
     );
   } else {
