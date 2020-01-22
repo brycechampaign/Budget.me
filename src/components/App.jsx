f6272cef9efb92101464b6c9752659c939d21f06
Bryce Champaign <bechampaign@protonmail.com>
2020-01-22 00:09:18 -0500

    Replace App.jsx code with routing logic
    The code in the App component has been moved to a new component called "Home", while the App component is now used for client-side routing. "Home" is a more appropriate name for the
    function of the App component, and the logic for React Router should be at the top level.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,114 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { getTransactionsForMonth, getMonthlyBudget } from '../server-helpers';
-import Overview from './Overview';
-import HistoryChart from './HistoryChart';
-import CategoryChart from './CategoryChart';
-import TxContainer from './TxContainer';
 import BudgetForm from './BudgetForm';
+import Home from './Home';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 export default () => {
-  const [transactions, setTransactions] = useState([]);
-  const [user, setUser] = useState(null);
-  const [monthlyBudget, setMonthlyBudget] = useState(null);
-  const [month, setMonth] = useState(null);
-  const [totalSpent, setTotalSpent] = useState(null);
-  const [income, setIncome] = useState(null);
-  const [categories, setCategories] = useState({});
-
-  const updateTransactions = () => {
-    return getTransactionsForMonth(user, month).then(transactions =>
-      setTransactions(transactions)
-    );
-  };
-
-  const updateMonthlyBudget = () => {
-    getMonthlyBudget(user, month).then(budget => setMonthlyBudget(budget));
-  };
-
-  const getStats = () => {
-    let income = 0;
-    let outcome = 0;
-    const categoriesTemp = {};
-
-    transactions.forEach(tx => {
-      const { amount, category } = tx;
-
-      if (category === 'income') {
-        income += amount;
-      } else {
-        const categoryAmount = categoriesTemp[category];
-
-        if (categoryAmount === undefined) {
-          categoriesTemp[category] = amount;
-        } else {
-          categoriesTemp[category] = categoryAmount + amount;
-        }
-
-        outcome += amount;
-      }
-    });
-
-    setCategories(categoriesTemp);
-    setIncome(income);
-    setTotalSpent(outcome);
-  };
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
 
   useEffect(() => {
-    setMonth(new Date().getMonth());
-    updateMonthlyBudget();
-    updateTransactions();
-
-    if (user === null) {
-      setUser(localStorage.getItem('user', user) || null);
-    } else {
-      localStorage.setItem('user', user);
-    }
+    localStorage.setItem('user', JSON.stringify(user));
   }, [user]);
 
-  useEffect(() => {
-    if (month !== null) {
-      updateTransactions();
-      updateMonthlyBudget();
-    }
-  }, [month]);
-
-  useEffect(() => {
-    if (transactions) {
-      getStats();
-    }
-  }, [transactions]);
-
-  if (user === null) {
-    return <BudgetForm setUser={setUser} />;
-  } else {
-    return (
-      <>
-        <h1>
-          Welcome back, <span id="username">{user}</span>
-        </h1>
-        <Overview
-          budget={monthlyBudget}
-          totalSpent={totalSpent}
-          income={income}
-        />
-        <TxContainer
-          transactions={transactions}
-          user={user}
-          updateTransactions={updateTransactions}
-        />
-        {transactions !== null &&
-        transactions.length > 0 &&
-        monthlyBudget !== null &&
-        transactions ? (
-          <HistoryChart transactions={transactions} budget={monthlyBudget} />
-        ) : null}
-        {transactions !== null &&
-        transactions.length > 0 &&
-        transactions &&
-        Object.keys(categories).length > 0 ? (
-          <CategoryChart categoryMap={categories} totalSpent={totalSpent} />
-        ) : null}
-      </>
-    );
-  }
+  return (
+    <BrowserRouter>
+      <Route exact path="/">
+        {user ? <Redirect to={`/${user}`} /> : <Redirect to="/login" />}
+      </Route>
+      <Switch>
+        <Route path="/login">
+          <BudgetForm setUser={setUser} />
+          {user ? <Redirect to={`/${user}`} /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/:user">
+          {user ? <Home user={user} /> : <Redirect to="/login" />}
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
